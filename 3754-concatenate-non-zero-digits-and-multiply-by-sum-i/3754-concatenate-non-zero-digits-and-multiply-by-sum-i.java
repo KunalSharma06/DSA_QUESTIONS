875ea07198926fc19c13366e580910c27c1bbcc1
Kunal Sharma <sharmakunal19806@gmail.com>
2026-07-07 14:26:44 +0530

    Time: 1 ms (99.85%), Space: 42.5 MB (67.71%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.java
@@ -1,20 +1,47 @@
 class Solution {
-    public long sumAndMultiply(int n) {
-        String str = "";
-        String s = "" + n;
-        for(int i = 0; i < s.length(); i++){
-            if(s.charAt(i) != '0'){
-                str += s.charAt(i);
-            }
+    // public long sumAndMultiply(int n) {
+    //     String str = "";
+    //     String s = "" + n;
+    //     for(int i = 0; i < s.length(); i++){
+    //         if(s.charAt(i) != '0'){
+    //             str += s.charAt(i);
+    //         }
+    //     }
+    //     if(str.isEmpty()) return 0;
+    //     long ans = Long.parseLong(str);
+    //     int sum = 0;
+    //     long digit = ans;
+    //     while(digit != 0){
+    //         sum += digit % 10;
+    //         digit /= 10;
+    //     }
+    //     return ans * sum;
+    // }
+
+    long x = 0;
+    long sum = 0;
+
+    public long sumAndMultiply(long n) {
+        x = 0;
+        sum = 0;
+
+        build(n);
+
+        return x * sum;
+    }
+
+    public void build(long n) {
+        if (n == 0) {
+            return;
         }
-        if(str.isEmpty()) return 0;
-        long ans = Long.parseLong(str);
-        int sum = 0;
-        long digit = ans;
-        while(digit != 0){
-            sum += digit % 10;
-            digit /= 10;
+
+        build(n / 10);
+
+        long digit = n % 10;
+
+        if (digit != 0) {
+            x = x * 10 + digit;
+            sum += digit;
         }
-        return ans * sum;
     }
 }
