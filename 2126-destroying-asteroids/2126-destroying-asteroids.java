e344288787cfd47e4baa644c11e20fe3f1344f6e
Kunal Sharma <sharmakunal19806@gmail.com>
2026-07-08 16:40:45 +0530

    Time: 27 ms (19.26%), Space: 107.4 MB (51.02%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.java
+++ 2126-destroying-asteroids/2126-destroying-asteroids.java
@@ -2,17 +2,27 @@ class Solution {
     public boolean asteroidsDestroyed(int mass, int[] asteroids) {
         Arrays.sort(asteroids);
         long sum = mass;
+        // for(int i = 0; i < asteroids.length; i++){
+        //     if(asteroids[i] <= sum){
+        //         sum += asteroids[i];
+        //         asteroids[i] = 0;
+        //     }else{
+        //         asteroids[i] = -1;
+        //     }
+        // }
+
+        // for(int i = 0; i < asteroids.length; i++){
+        //     if(asteroids[i] != 0){
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+
         for(int i = 0; i < asteroids.length; i++){
             if(asteroids[i] <= sum){
                 sum += asteroids[i];
-                asteroids[i] = 0;
             }else{
-                asteroids[i] = -1;
-            }
-        }
-
-        for(int i = 0; i < asteroids.length; i++){
-            if(asteroids[i] != 0){
                 return false;
             }
         }
