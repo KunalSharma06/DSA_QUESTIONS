ce8c55a0808067bf37e2d3f40d6ce9ab72faa587
Kunal Sharma <sharmakunal19806@gmail.com>
2026-07-23 12:11:54 +0530

    Time: 5 ms (71.2%), Space: 168.7 MB (64.2%) - LeetHub

---
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean mergeTriplets(int[][] triplets, int[] target) {
+        int n = triplets.length;
+        boolean first = false, second = false, third = false;
+        for(int i = 0; i < n; i++){
+            if(triplets[i][0] > target[0] || triplets[i][1] > target[1] || triplets[i][2] > target[2]){
+                continue;
+            }
+            if(triplets[i][0] == target[0]) first = true;
+            if(triplets[i][1] == target[1]) second = true;
+            if(triplets[i][2] == target[2]) third = true;
+        }
+        if(first && second && third){
+            return true;
+        }
+        return false;
+    }
+}
