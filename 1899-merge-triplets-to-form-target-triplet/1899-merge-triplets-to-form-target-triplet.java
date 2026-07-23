a033efcb1a7d560301cacf35df77118e2f61320a
Kunal Sharma <sharmakunal19806@gmail.com>
2026-07-23 12:12:53 +0530

    Time: 1 ms (99.7%), Space: 168.9 MB (29.51%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.java
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.java
@@ -9,6 +9,7 @@ class Solution {
             if(triplets[i][0] == target[0]) first = true;
             if(triplets[i][1] == target[1]) second = true;
             if(triplets[i][2] == target[2]) third = true;
+            if(first && second && third) break;
         }
         if(first && second && third){
             return true;
