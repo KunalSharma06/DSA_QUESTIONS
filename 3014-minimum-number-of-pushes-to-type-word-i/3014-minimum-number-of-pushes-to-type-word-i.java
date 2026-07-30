8b76c0595e7219128a9f1f2c166e2ae360019e22
Kunal Sharma <sharmakunal19806@gmail.com>
2026-07-31 00:34:41 +0530

    Time: 1 ms (29.67%), Space: 43.2 MB (47.13%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,19 +1,36 @@
 class Solution {
     public int minimumPushes(String word) {
-        int[] freq = new int[26];
+        // int[] freq = new int[26];
+        // for(char ch : word.toCharArray()){
+        //     freq[ch - 'a']++;
+        // }
+        // Arrays.sort(freq);
+        // int ans = 0;
+        // int count = 0;
+        // for(int i = 25; i >= 0; i--){
+        //     if(freq[i] == 0){
+        //         continue;
+        //     }
+        //     ans += freq[i] * (count / 8 + 1);
+        //     count++;
+        // }
+        // return ans;
+
+        Set<Character> set = new HashSet<>();
+        int pushes = 1;
+        int pushesCount = 0;
+        int setSize = 0;
         for(char ch : word.toCharArray()){
-            freq[ch - 'a']++;
+            if(setSize == 8){
+                setSize = 0;
+                pushes++;  
+            } 
+
+            set.add(ch);
+            setSize++;
+            pushesCount += pushes;   
         }
-        Arrays.sort(freq);
-        int ans = 0;
-        int count = 0;
-        for(int i = 25; i >= 0; i--){
-            if(freq[i] == 0){
-                continue;
-            }
-            ans += freq[i] * (count / 8 + 1);
-            count++;
-        }
-        return ans;
+
+        return pushesCount;
     }
 }
