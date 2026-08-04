b3980be6ec8af02ba0cde16db97e0ada5b1d376c
Kunal Sharma <sharmakunal19806@gmail.com>
2026-08-04 20:48:13 +0530

    Time: 1 ms (100%), Space: 46.8 MB (47.61%) - LeetHub

--- 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.java
+++ 3917-count-indices-with-opposite-parity/3917-count-indices-with-opposite-parity.java
@@ -2,22 +2,43 @@ class Solution {
     public int[] countOppositeParity(int[] nums) {
         int n = nums.length;
         int[] arr = new int[n];
+        // for(int i = 0; i < nums.length; i++){
+        //     int val1 = nums[i];
+        //     int score = 0;
+        //     for(int j = i + 1; j < nums.length; j++){
+        //         int val2 = nums[j];
+        //         if(val1 % 2 == 0){
+        //             if(val2 % 2 != 0){
+        //                 score++;
+        //             }
+        //         }else{
+        //             if(val2 % 2 == 0){
+        //                 score++;
+        //             }
+        //         }
+        //     }
+        //     arr[i] = score;
+        // }
+        // return arr;
+
+        int odd = 0;
+        int even = 0;
         for(int i = 0; i < nums.length; i++){
-            int val1 = nums[i];
-            int score = 0;
-            for(int j = i + 1; j < nums.length; j++){
-                int val2 = nums[j];
-                if(val1 % 2 == 0){
-                    if(val2 % 2 != 0){
-                        score++;
-                    }
-                }else{
-                    if(val2 % 2 == 0){
-                        score++;
-                    }
-                }
+            if(nums[i] % 2 == 0){
+                even++;
+            }else{
+                odd++;
             }
-            arr[i] = score;
+        }
+
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] % 2 == 0){
+                arr[i] = odd;
+                even--;
+            }else{
+                arr[i] = even;
+                odd--;
+            }
         }
         return arr;
     }
