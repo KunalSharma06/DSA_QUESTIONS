2bce2fff571ddd98eebf82d87b8c7d717d59272d
Kunal Sharma <sharmakunal19806@gmail.com>
2026-08-05 20:14:05 +0530

    Time: 1 ms (99.3%), Space: 46.4 MB (32.49%) - LeetHub

--- 3866-first-unique-even-element/3866-first-unique-even-element.java
+++ 3866-first-unique-even-element/3866-first-unique-even-element.java
@@ -1,17 +1,31 @@
 class Solution {
     public int firstUniqueEven(int[] nums) {
-        int count = 0;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++){
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-        }
-        int value = -1;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] % 2 == 0 && map.get(nums[i]) == 1){
-                value = nums[i];
-                break;
+        // int count = 0;
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for(int i = 0; i < nums.length; i++){
+        //     map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        // }
+        // int value = -1;
+        // for(int i = 0; i < nums.length; i++){
+        //     if(nums[i] % 2 == 0 && map.get(nums[i]) == 1){
+        //         value = nums[i];
+        //         break;
+        //     }
+        // }
+        // return value;
+
+        int[] arr = new int[200];
+        for(int num : nums){
+            if(num % 2 == 0){
+                arr[num]++;
             }
         }
-        return value;
+
+        for(int i = 0; i < nums.length; i++){
+            if(arr[nums[i]] == 1 && nums[i] % 2 == 0){
+                return nums[i];
+            }
+        }
+        return -1;
     }
 }
