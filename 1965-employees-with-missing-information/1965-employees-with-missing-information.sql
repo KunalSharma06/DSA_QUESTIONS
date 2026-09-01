7b06c6c4c08373a25a6df81303968d51de99597b
Kunal Sharma <sharmakunal19806@gmail.com>
2026-09-01 15:13:44 +0530

    Time: 730 ms (23%), Space: 0B (100%) - LeetHub

---
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+
+select employee_id from 
+(
+    select employee_id from employees
+    union all
+    select employee_id from salaries
+) as t
+group by employee_id
+having count(*) = 1
+order by employee_id asc;
