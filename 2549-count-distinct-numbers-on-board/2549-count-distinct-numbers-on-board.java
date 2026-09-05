827feae59f99dfec793853d3946ff9dfb5e2663e
Kunal Sharma <sharmakunal19806@gmail.com>
2026-09-05 20:12:49 +0530

    Time: 25 ms (2%), Space: 43.6 MB (9.55%) - LeetHub

--- 2549-count-distinct-numbers-on-board/2549-count-distinct-numbers-on-board.java
+++ 2549-count-distinct-numbers-on-board/2549-count-distinct-numbers-on-board.java
@@ -1,6 +1,29 @@
 class Solution {
     public int distinctIntegers(int n) {
-        if(n == 1) return 1;
-        return n - 1;
+        // if(n == 1) return 1;
+        // return n - 1;
+
+        Set<Integer> set = new HashSet<>();
+        set.add(n);
+
+        boolean changed = true;
+
+        while (changed) {
+            changed = false;
+            List<Integer> current = new ArrayList<>(set);
+
+            for (int x : current) {
+
+                for (int i = 1; i <= n; i++) {
+
+                    if (x % i == 1 && !set.contains(i)) {
+                        set.add(i);
+                        changed = true;
+                    }
+                }
+            }
+        }
+
+        return set.size();
     }
 }
