2f11997dee70d2be03b43ab6cc9e1f43490cd28b
Kunal Sharma <sharmakunal19806@gmail.com>
2026-09-06 21:29:33 +0530

    Time: 1 ms (100%), Space: 43.7 MB (87.41%) - LeetHub

--- 4043-count-rotations-with-exactly-k-equal-adjacent-pairs/4043-count-rotations-with-exactly-k-equal-adjacent-pairs.java
+++ 4043-count-rotations-with-exactly-k-equal-adjacent-pairs/4043-count-rotations-with-exactly-k-equal-adjacent-pairs.java
@@ -1,21 +1,44 @@
+// class Solution {
+//     public int countRotations(String s, int k) {
+//         int n = s.length();
+//         int ans = 0;
+//         for(int r = 0; r < n; r++){
+//             int score = 0;
+//             for(int i = 0; i < n - 1; i++){
+//                 char a = s.charAt((r + i) % n);
+//                 char b = s.charAt((r + i + 1) % n);
+
+//                 if(a == b){
+//                     score++;
+//                 }
+//             }
+//             if(score == k){
+//                 ans++;
+//             }
+//         }
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int countRotations(String s, int k) {
         int n = s.length();
-        int ans = 0;
-        for(int r = 0; r < n; r++){
-            int score = 0;
-            for(int i = 0; i < n - 1; i++){
-                char a = s.charAt((r + i) % n);
-                char b = s.charAt((r + i + 1) % n);
+        int equal = 0;
 
-                if(a == b){
-                    score++;
-                }
-            }
-            if(score == k){
-                ans++;
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == s.charAt((i + 1) % n)) {
+                equal++;
             }
         }
-        return ans;
+
+        if (k == equal - 1) {
+            return equal;
+        }
+
+        if (k == equal) {
+            return n - equal;
+        }
+
+        return 0;
     }
 }
