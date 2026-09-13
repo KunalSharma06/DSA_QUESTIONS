e25cdd4a8ad37aa70c4d01aca02114adf0c5ab1a
Kunal Sharma <sharmakunal19806@gmail.com>
2026-09-13 19:37:35 +0530

    Time: 7 ms (56.7%), Space: 47.2 MB (33.26%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int n = numCourses;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < n; i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int i = 0; i < prerequisites.length; i++){
+            adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
+        }
+        int[] indegree = new int[n];
+        for(int i = 0; i < n; i++){
+            for(int it : adj.get(i)){
+                indegree[it]++;
+            }
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < n; i++){
+            if(indegree[i] == 0){
+                q.add(i);
+            }
+        }
+        int cnt = 0;
+        while(!q.isEmpty()){
+            int node = q.peek();
+            q.remove();
+            cnt++;
+
+            for(int it : adj.get(node)){
+                indegree[it]--;
+                if(indegree[it] == 0){
+                    q.add(it);
+                }
+            }
+        }
+        if(cnt == n) return true;
+        return false;
+    }
+}
