d846c85f79178112026664c5b0bc1494c0a8ac99
Kunal Sharma <sharmakunal19806@gmail.com>
2026-09-13 19:38:31 +0530

    Time: 7 ms (56.7%), Space: 47.4 MB (21.25%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -6,7 +6,7 @@ class Solution {
             adj.add(new ArrayList<>());
         }
         for(int i = 0; i < prerequisites.length; i++){
-            adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
+           adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
         }
         int[] indegree = new int[n];
         for(int i = 0; i < n; i++){
